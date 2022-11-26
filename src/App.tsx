c6e9940f7d65ad736e735b0bd0a0a6210e5893d8
JoelJuniorManuelFombe <joeljuniormanuel@gmail.com>
2022-11-26 03:01:52 +0200

    actualizacao

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Corpo } from "./components/corpo/corpoSection"
 export default function App() {
 
   return (
-    <div className="App bg-[#F3F5F7]">
+    <div className="App bg-[#F3F5F7] text-body">
         <NavBar/>
         <Corpo/>
     </div>
